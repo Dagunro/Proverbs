a7310e8af209b0c0f82828801240716f775fb9d2
tunjiarofin <tunjiarofin@gmail.com>
2025-03-01 16:45:19 +0100

    Building UI

--- app/src/main/java/com/example/proverbs/MainActivity.kt
+++ app/src/main/java/com/example/proverbs/MainActivity.kt
@@ -4,22 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.proverbs.ui.theme.ProverbsTheme
@@ -48,11 +56,25 @@ fun ProverbsApp(){
 @Composable
 fun ProverbsQuote()
 {
+
+
+
     Surface(
         modifier = Modifier
             .fillMaxSize(),
 
     ) {
+
+//        Box(
+//            modifier = Modifier
+//                .size(230.dp, 23.dp)
+//                .background(color = Color.Yellow),
+//        ){
+//            Text(
+//                text = "Proverbs."
+//            )
+//        }
+
         Column (
             modifier = Modifier.fillMaxSize().padding(25.dp),
             verticalArrangement = Arrangement.Center
@@ -78,12 +100,31 @@ fun ProverbsQuote()
 
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(30.dp))
 
             Button(onClick = {}) {
-
+                Text(text = "Previous")
             }
         }
+    }
+    Spacer(modifier = Modifier.height(350.dp))
+    Box(
+        modifier = Modifier
+            .size(170.dp, 130.dp)
+            .padding(16.dp)
+            .background(color = Color.Yellow),
+
+
+        contentAlignment = Alignment.CenterStart
+    ){
+
+        Text(
+            text = "Proverbs.",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+
+
+        )
     }
 
 
